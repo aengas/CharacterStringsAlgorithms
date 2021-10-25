ad223b61cd7b73989b2042116be37beabf26b948
Asbjørn Engås <aen@dips.no>
2021-10-25 08:28:44 +0200

    Maximal Boundaries

---
+++ CharacterStringsAlgorithms.Tests/MaximalBoundariesTests.cs
@@ -0,0 +1,85 @@
+﻿using CharacterStringsAlgorithms.MaximalBoundaries;
+using Xunit;
+
+namespace CharacterStringsAlgorithms.Tests
+{
+    public class MaximalBoundariesTests
+    {
+        [Fact]
+        public void MaximalBorderLengthTest1()
+        {
+            string input = "abaababaa";
+            int[] expectedOutput = { 0, 0, 1, 1, 2, 3, 2, 3, 4 };
+
+            int[] output = MaximalBoundariesCalculator.MaximumBorderLength(input);
+            
+            Assert.Equal(expectedOutput, output);
+        }
+
+        [Fact]
+        public void KnuthMorrisPrattTest1()
+        {
+            string input1 = "supercalifragilisticexpialidocious";
+            string input2 = "fragil";
+            int expectedOutput = 9;
+
+            int output = MaximalBoundariesCalculator.KnuthMorrisPratt(input1, input2);
+
+            Assert.Equal(expectedOutput, output);
+        }
+
+        [Fact]
+        public void KnuthMorrisPrattTest2()
+        {
+            string input1 = "ababaabaa";
+            string input2 = "abaa";
+            int expectedOutput = 2;
+
+            int output = MaximalBoundariesCalculator.KnuthMorrisPratt(input1, input2);
+
+            Assert.Equal(expectedOutput, output);
+        }
+
+        [Fact]
+        public void PowerstringTest1()
+        {
+            string input = "blablabla";
+            int expectedOutput = 3;
+
+            int output = MaximalBoundariesCalculator.PowerstringByBorder(input);
+
+            Assert.Equal(expectedOutput, output);
+        }
+
+        [Fact]
+        public void PowerstringTest2()
+        {
+            string input = "blablabla";
+            int expectedOutput = 3;
+
+            int output = MaximalBoundariesCalculator.PowerstringByFind(input);
+
+            Assert.Equal(expectedOutput, output);
+        }
+
+        // https://open.kattis.com/problems/powerstrings
+        [Fact]
+        public void PowerstringTest3()
+        {
+            Assert.Equal(1, MaximalBoundariesCalculator.PowerstringByFind("abcd"));
+            Assert.Equal(4, MaximalBoundariesCalculator.PowerstringByFind("aaaa"));
+            Assert.Equal(3, MaximalBoundariesCalculator.PowerstringByFind("ababab"));
+        }
+
+        [Fact]
+        public void ConjugateTest1()
+        {
+            string input1 = "sweetsour";
+            string input2 = "soursweet";
+
+            bool output = MaximalBoundariesCalculator.Conjugate(input1, input2);
+
+            Assert.True(output);
+        }
+    }
+}
---
+++ CharacterStringsAlgorithms/MaximalBoundaries/MaximalBoundariesCalculator.cs
@@ -0,0 +1,85 @@
+﻿using System;
+
+namespace CharacterStringsAlgorithms.MaximalBoundaries
+{
+    public class MaximalBoundariesCalculator
+    {
+        public static int[] MaximumBorderLength(string w)
+        {
+            int n = w.Length;
+
+            var f = new int[n];                         // init f[0] = 0
+            int k = 0;                                  // nåværende lengste grenselengde
+            for (int i = 1; i < n; i++)                 // beregn f[i]
+            {
+                while (w[k] != w[i] && k > 0)
+                {
+                    k = f[k - 1];                       // mismatch: prøv neste grense
+                }
+
+                if (w[k] == w[i])                       // siste bokstaver matcher
+                {
+                    k++;                                // vi kan inkrementere grenselengden
+                }
+
+                f[i] = k;                               // vi har funnet den maksimale grensen til w[:i + 1]    
+            }
+
+            return f;
+        }
+
+        public static int KnuthMorrisPratt(string s, string t)
+        {
+            char sep = '\x00';                          // spesielt ubrukt tegn
+            if (s.Contains(sep) || t.Contains(sep))
+            {
+                throw new Exception("strengene inneholder separator");
+            }
+            
+            int[] f = MaximumBorderLength(t + sep + s);
+
+            int n = t.Length;
+
+            for (int i = 0; i < f.Length; i++)
+            {
+                if (f[i] == n)                          // fant en grense med lengde t
+                {
+                    return i - 2 * n;                   // begynnelsen på grensen i s
+                }
+            }
+
+            return -1;
+        }
+
+        // Denne fungerte i https://open.kattis.com/problems/powerstrings
+        public static int PowerstringByBorder(string u)
+        {
+            int[] f = MaximumBorderLength(u);
+
+            int n = u.Length;
+
+            if (n % (n - f[^1]) == 0)                   // deler justeringsforskyvningen seg i n ?
+            {
+                return n / (n - f[^1]);                 // vi har funnet en potens-dekomposisjon
+            }
+
+            return 1;
+        }
+
+        // Denne timet ut i https://open.kattis.com/problems/powerstrings
+        public static int PowerstringByFind(string u)
+        {
+            return u.Length / (u + u).IndexOf(u, 1);
+        }
+
+        public static bool Conjugate(string x, string y)
+        {
+            if (x.Length != y.Length)
+            {
+                return false;
+            }
+            
+            return KnuthMorrisPratt(x + x, y) >= 0;
+        }
+    }
+}
