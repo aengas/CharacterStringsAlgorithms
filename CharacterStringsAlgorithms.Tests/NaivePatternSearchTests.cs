f4154e1f0ec85522415a547ad45d4febe23f671f
Asbjørn Engås <aen@dips.no>
2021-11-14 11:38:18 +0100

    Naivt mønstersøk

---
+++ CharacterStringsAlgorithms.Tests/NaivePatternSearchTests.cs
@@ -0,0 +1,56 @@
+﻿using CharacterStringsAlgorithms.NaivePatternSearch;
+using Xunit;
+
+namespace CharacterStringsAlgorithms.Tests
+{
+    public class NaivePatternSearchTests
+    {
+        [Fact]
+        public void NaivePatternSearchTest1()
+        {
+            const string InputString = "lalopalalali";
+            const string InputPattern = "lala";
+            const int ExpectedOutput = 6;
+
+            int output = NaivePatternSearcher.Search(InputString, InputPattern);
+
+            Assert.Equal(ExpectedOutput, output);
+        }
+
+        [Fact]
+        public void NaivePatternSearchTest2()
+        {
+            const string InputString = "lalopalala";
+            const string InputPattern = "lala";
+            const int ExpectedOutput = 6;
+
+            int output = NaivePatternSearcher.Search(InputString, InputPattern);
+
+            Assert.Equal(ExpectedOutput, output);
+        }
+        
+        [Fact]
+        public void NaivePatternSearchTest3()
+        {
+            const string InputString = "lalopalila";
+            const string InputPattern = "lala";
+            const int ExpectedOutput = -1;
+
+            int output = NaivePatternSearcher.Search(InputString, InputPattern);
+
+            Assert.Equal(ExpectedOutput, output);
+        }
+
+        [Fact]
+        public void NaivePatternSearchTest4()
+        {
+            const string InputString = "THIS IS A TEST TEXT";
+            const string InputPattern = "TEST";
+            const int ExpectedOutput = 10;
+
+            int output = NaivePatternSearcher.Search(InputString, InputPattern);
+
+            Assert.Equal(ExpectedOutput, output);
+        }
+    }
+}
---
+++ CharacterStringsAlgorithms/NaivePatternSearch/NaivePatternSearcher.cs
@@ -0,0 +1,29 @@
+﻿namespace CharacterStringsAlgorithms.NaivePatternSearch
+{
+    public class NaivePatternSearcher
+    {
+        public static int Search(string s, string t)
+        {
+            int n = s.Length;
+            int m = t.Length;
+            for (int i = 0; i <= n - m; i++)
+            {
+                int j;
+                for (j = 0; j < m; j++)
+                {
+                    if (t[j] != s[i + j])
+                    {
+                        break;
+                    }
+                }
+
+                if (j == m)
+                {   
+                    return i;
+                }
+            }
+
+            return -1;
+        }
+    }
+}
